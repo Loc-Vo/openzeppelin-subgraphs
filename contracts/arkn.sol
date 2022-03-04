883054942abf85ea11fc5de9fb9ea42b1537c1df
Loc-Vo <voducloc@gmail.com>
2022-03-04 14:03:47 +0700

    update smc addresses

--- contracts/arkn.sol
+++ contracts/arkn.sol
@@ -1,9 +1,20 @@
-// SPDX-License-Identifier: MIT
+    // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts@4.5.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract ArkRivals is ERC721 {
+contract ArkRivals is ERC721, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
     constructor() ERC721("Ark Rivals", "ARKN") {}
-}
 
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+}
